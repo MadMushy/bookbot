2f4559596e464ae0d377f143f00ec8efc89e43ee
da5734 <davidhels92@proton.me>
2025-06-14 04:40:01 -0700

    reading book from file

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("greetings boots")
+
+
+# file_path = "/home/david/bootdev_projects/bookbot/books/frankenstein.txt"
+
+
+def get_book_text(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+        return file_contents
+
+
+def main():
+
+    file_path = "/home/david/bootdev_projects/bookbot/books/frankenstein.txt"
+
+    book_content = get_book_text(file_path)
+    print(book_content)
+
+main()
+
