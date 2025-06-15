1bb500871d1d8ea95235b9103ad1832a4806b3eb
da5734 <davidhels92@proton.me>
2025-06-15 01:19:12 -0700

    counting char's in lowercase

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
-
-
-# file_path = "/home/david/bootdev_projects/bookbot/books/frankenstein.txt"
-
+from stats import count_words, char_counts
 
 def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
-        return file_contents
-
+        return file_contents 
 
 def main():
 
     file_path = "/home/david/bootdev_projects/bookbot/books/frankenstein.txt"
 
     book_content = get_book_text(file_path)
-    print(book_content)
+
+    book_word_count = count_words(book_content)
+
+    book_char_count = char_counts(book_content) #try putting in here instead
+
+    print(f"{book_word_count} words found in the document")
+
+    print(f"{book_char_count}")
+
 
 main()
 
---
+++ stats.py
@@ -0,0 +1,15 @@
+def count_words(file_contents):
+    word_list = file_contents.split()
+    word_count = len(word_list)
+    return word_count
+
+
+def char_counts(file_contents):
+    char_count = {}
+    char_lower = file_contents.lower()
+    for char in char_lower:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count 
